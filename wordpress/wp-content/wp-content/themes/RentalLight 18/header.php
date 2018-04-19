3e8cc839f971cd67a717c62303e0972da7918823
Alex Kusick <Akusick@mcad.edu>
2018-04-18 20:42:26 -0500

    updates

---
+++ wordpress/wp-content/wp-content/themes/RentalLight 18/header.php
@@ -0,0 +1,24 @@
+<!-- <?echo basename(__FILE__); ?> -->
+<html>
+	<head>
+		<title>IDM 6630 - Development Docker Container</title>
+		<?php wp_head(); ?>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<link rel="stylesheet" href="style.css">
+		<link href="https://fonts.googleapis.com/css?family=Junge|Poppins:500" rel="stylesheet">
+
+		<?php wp_head(); ?>
+	</head>
+
+	<body>
+		 <header>
+		 	<?php get_template_part('nav'); ?>
+		 	<h1><?php echo get_bloginfo ('name'); ?></h1>
+			<!--<?php echo get_bloginfo ('decription'); ?>-->
+
+		 </header>
+
+<!-- <?php echo "END OF" . basename(__FILE__); ?> -->
+
---
+++ wordpress/wp-content/wp-content/themes/RentalLight 18/single.php
@@ -0,0 +1,19 @@
+<!-- <?echo basename(__FILE__); ?>  -->
+
+<?php define( 'WP_USE_THEMES', false ); get_header(); ?>
+
+	<body <?php body_class(); ?>>
+
+		<header>
+			<?php get_template_part('main-single'); ?>
+		</header>
+
+
+		<footer>
+			<?php get_footer(); ?> 
+		</footer>
+
+	</body>
+
+<!-- <?php echo "END OF" . basename(__FILE__); ?>  -->
+
